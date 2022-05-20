84be50997c9dc736953e79069c64e061acd6fd78
vera <lerika250293@mail.ru>
2022-05-20 14:49:17 +0300

    cheack

---
+++ connector.py
@@ -0,0 +1,50 @@
+
+class DiskConnector:
+    def __init__(self):
+         pass
+    def upload_date(self, bot, message):
+        total = []
+        message_to_save_date = message.text
+        for el in message_to_save_date:
+            if el.isdigit():
+                total.append(el)
+        if len(total) == 3:
+            total = total[0] + '.' + total[1] + total[2]
+            return total
+        elif len(total) == 4:
+            total = total[0] + total[1] +'.' + total[2] + total[3]
+            return total
+        else:
+            return False
+
+    def upload_time(self, bot, message):
+        total = []
+        message_to_save_time = message.text
+        for el in message_to_save_time:
+            if el.isdigit():
+                total.append(el)
+        if len(total) == 3:
+            total = total[0] + '-' + total[1] + total[2]
+            return total
+        elif len(total) == 4:
+            total = total[0] + total[1] +'-' + total[2] + total[3]
+            return total
+        elif len(total) == 8:
+            total = total[0] + total[1] + ':' + total[2] + total[3] + '-' + total[4] + total[5] + ':' + total[6] + total[7]
+            return total
+        else:
+            return False
+
+    def upload_number(self, bot, message):
+        message_to_save_number = message.text
+        if message_to_save_number.isdigit():
+            return message_to_save_number
+        else:
+            return False
+
+    def back(self, bot, message):
+        message_to_back = message.text
+        if message_to_back == 'Назад в меню':
+            return message_to_back
+        else:
+            return False
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import re
 import telebot
 from telebot import types
 from config import id_chanel, token
+from connector import DiskConnector
 bot = telebot.TeleBot(token)
 
 change_list = []
 change_time_list = []
 up_date_weekend = []
-
+test = DiskConnector()
 @bot.message_handler(commands=['start'])
 def start(message):
     menu = types.ReplyKeyboardMarkup(row_width = 1, resize_keyboard=True)
@@ -17,9 +17,6 @@ def start(message):
     menu.add(change_button, change_time_button,up_date_weekend_button)
     bot.send_message(message.chat.id, 'Выберите параметр!', reply_markup=menu)
 
-@bot.message_handler(func=lambda m: m.text == 'Назад в меню')
-def back(message):
-    start(message)
 
 @bot.message_handler(func=lambda m: m.text == 'Замена')
 def change(message):
@@ -28,27 +25,19 @@ def change(message):
     catalog_keyboard.add(back_button)
     sent_1 = bot.reply_to(message, 'Пожалуйста, оставьте дату замены. 01.01', reply_markup=catalog_keyboard)
     bot.register_next_step_handler(sent_1, date)
+
 @bot.message_handler(func=lambda m: m.text == 'Назад в меню')
 def back(message):
     start(message)
 
 def date(message):
-    total = []
-    if message.text == 'Назад в меню':
+    if test.back(bot, message):
         start(message)
     else:
-        message_to_save_date = message.text
-        for el in message_to_save_date:
-            if el.isdigit():
-                total.append(el)
-        if len(total) == 3:
-            total = total[0] + '.' + total[1] + total[2]
-            change_list.append(total)
-            sent_2 = bot.reply_to(message, 'Пожалуйста, введите время смены. 10:00-23:00 с указанием часов и минут')
-            bot.register_next_step_handler(sent_2, time)
-        elif len(total) == 4:
-            total = total[0] + total[1] +'.' + total[2] + total[3]
-            change_list.append(total)
+        if test.upload_date(bot, message):
+            upload_result = test.upload_date(bot, message)
+            change_list.append(upload_result)
+            print(change_list)
             sent_2 = bot.reply_to(message, 'Пожалуйста, введите время смены. 10:00-23:00 с указанием часов и минут')
             bot.register_next_step_handler(sent_2, time)
         else:
@@ -56,27 +45,12 @@ def date(message):
             bot.register_next_step_handler(sent_5, date)
 
 def time(message):
-    total = []
-    if message.text == 'Назад в меню':
+    if test.back(bot, message):
         start(message)
     else:
-        message_to_save_time = message.text
-        for el in message_to_save_time:
-            if el.isdigit():
-                total.append(el)
-        if len(total) == 3:
-            total = total[0] + '-' + total[1] + total[2]
-            change_list.append(total)
-            sent_2 = bot.reply_to(message, 'Пожалуйста, свой бейдж сотрудника! 01234')
-            bot.register_next_step_handler(sent_2, number_myself)
-        elif len(total) == 4:
-            total = total[0] + total[1] +'-' + total[2] + total[3]
-            change_list.append(total)
-            sent_2 = bot.reply_to(message, 'Пожалуйста, свой бейдж сотрудника! 01234')
-            bot.register_next_step_handler(sent_2, number_myself)
-        elif len(total) == 8:
-            total = total[0] + total[1] + ':' + total[2] + total[3] + '-' + total[4] + total[5] + ':' + total[6] + total[7]
-            change_list.append(total)
+        if test.upload_time(bot, message):
+            upload_result = test.upload_time(bot, message)
+            change_list.append(upload_result)
             sent_2 = bot.reply_to(message, 'Пожалуйста, свой бейдж сотрудника! 01234')
             bot.register_next_step_handler(sent_2, number_myself)
         else:
@@ -84,34 +58,34 @@ def time(message):
             bot.register_next_step_handler(sent_5, time)
 
 def number_myself(message):
-     if message.text == 'Назад в меню':
+     if test.back(bot, message):
         start(message)
      else:
-        message_to_save_number = message.text
-        if message_to_save_number.isdigit():
-            change_list.append(message_to_save_number)
+         if test.upload_number(bot, message):
+            upload_result = test.upload_number(bot, message)
+            change_list.append(upload_result)
             sent_3 = bot.reply_to(message, 'Пожалуйста, укажите свою Фамилию и Имя! Иванова Анна')
             bot.register_next_step_handler(sent_3, fio_no_work)
-        else:
+         else:
             sent_3 = bot.reply_to(message, 'Пожалуйста, введите корректно табельный номер. 012345')
             bot.register_next_step_handler(sent_3, number_myself)
 
 def fio_no_work(message):
-    if message.text == 'Назад в меню':
+    if test.back(bot, message):
         start(message)
     else:
-        message_to_save_fio_no_work = message.text.lower()
+        message_to_save_fio_no_work = message.text
         change_list.append(message_to_save_fio_no_work)
         sent_2 = bot.reply_to(message, 'Пожалуйста, бейдж сотрудника, КОТОРЫЙ БУДЕТ РАБОТАТЬ! 012345')
         bot.register_next_step_handler(sent_2, number_to_change)
 
 def number_to_change(message):
-    if message.text == 'Назад в меню':
+    if test.back(bot, message):
         start(message)
     else:
-        message_to_save_number_to_change = message.text
-        if message_to_save_number_to_change.isdigit():
-            change_list.append(message_to_save_number_to_change)
+        if test.upload_number(bot, message):
+            upload_result = test.upload_number(bot, message)
+            change_list.append(upload_result)
             sent_3 = bot.reply_to(message, 'Пожалуйста, укажите Фамилию и Имя сотрудника, КОТОРЫЙ БУДЕТ РАБОТАТЬ! Иванова Анна')
             bot.register_next_step_handler(sent_3, fio_to_work)
         else:
@@ -119,7 +93,7 @@ def number_to_change(message):
             bot.register_next_step_handler(sent_3, number_to_change)
 
 def fio_to_work(message):
-    if message.text == 'Назад в меню':
+    if test.back(bot, message):
         start(message)
     else:
         message_to_save_fio_to_work = message.text.lower()
@@ -128,7 +102,7 @@ def fio_to_work(message):
         bot.register_next_step_handler(sent_3, place)
 
 def place(message):
-    if message.text == 'Назад в меню':
+    if test.back(bot, message):
         start(message)
     else:
         message_to_save_place = message.text
@@ -157,22 +131,12 @@ def change(message):
     bot.register_next_step_handler(sent_1, date_1)
 
 def date_1(message):
-    total = []
-    if message.text == 'Назад в меню':
+    if test.back(bot, message):
         start(message)
     else:
-        message_to_save_date = message.text
-        for el in message_to_save_date:
-            if el.isdigit():
-                total.append(el)
-        if len(total) == 3:
-            total = total[0] + '.' + total[1] + total[2]
-            change_time_list.append(total)
-            sent_2 = bot.reply_to(message, 'Пожалуйста, введите время смены. 10:00-23:00 с указанием часов и минут')
-            bot.register_next_step_handler(sent_2, time_1)
-        elif len(total) == 4:
-            total = total[0] + total[1] +'.' + total[2] + total[3]
-            change_time_list.append(total)
+        if test.upload_date(bot, message):
+            upload_result = test.upload_date(bot, message)
+            change_time_list.append(upload_result)
             sent_2 = bot.reply_to(message, 'Пожалуйста, введите время смены. 10:00-23:00 с указанием часов и минут')
             bot.register_next_step_handler(sent_2, time_1)
         else:
@@ -180,27 +144,12 @@ def date_1(message):
             bot.register_next_step_handler(sent_5, date_1)
 
 def time_1(message):
-    total = []
-    if message.text == 'Назад в меню':
+    if test.back(bot, message):
         start(message)
     else:
-        message_to_save_time = message.text
-        for el in message_to_save_time:
-            if el.isdigit():
-                total.append(el)
-        if len(total) == 3:
-            total = total[0] + '-' + total[1] + total[2]
-            change_time_list.append(total)
-            sent_2 = bot.reply_to(message, 'Пожалуйста, свой бейдж сотрудника! 01234')
-            bot.register_next_step_handler(sent_2, number_myself_1)
-        elif len(total) == 4:
-            total = total[0] + total[1] +'-' + total[2] + total[3]
-            change_time_list.append(total)
-            sent_2 = bot.reply_to(message, 'Пожалуйста, свой бейдж сотрудника! 01234')
-            bot.register_next_step_handler(sent_2, number_myself_1)
-        elif len(total) == 8:
-            total = total[0] + total[1] + ':' + total[2] + total[3] + '-' + total[4] + total[5] + ':' + total[6] + total[7]
-            change_time_list.append(total)
+        if test.upload_time(bot, message):
+            upload_result = test.upload_time(bot, message)
+            change_time_list.append(upload_result)
             sent_2 = bot.reply_to(message, 'Пожалуйста, свой бейдж сотрудника! 01234')
             bot.register_next_step_handler(sent_2, number_myself_1)
         else:
@@ -208,12 +157,12 @@ def time_1(message):
             bot.register_next_step_handler(sent_5, time_1)
 
 def number_myself_1(message):
-    if message.text == 'Назад в меню':
+    if test.back(bot, message):
         start(message)
     else:
-        message_to_save_number = message.text
-        if message_to_save_number.isdigit():
-            change_time_list.append(message_to_save_number)
+        if test.upload_number(bot, message):
+            upload_result = test.upload_number(bot, message)
+            change_time_list.append(upload_result)
             sent_3 = bot.reply_to(message, 'Пожалуйста, укажите свою Фамилию и Имя! Иванова Анна')
             bot.register_next_step_handler(sent_3, fio_1)
         else:
@@ -221,7 +170,7 @@ def number_myself_1(message):
             bot.register_next_step_handler(sent_3, number_myself_1)
 
 def fio_1(message):
-    if message.text == 'Назад в меню':
+    if test.back(bot, message):
         start(message)
     else:
         message_to_save_fio_no_work = message.text.lower()
@@ -230,7 +179,7 @@ def fio_1(message):
         bot.register_next_step_handler(sent_3, place_1)
 
 def place_1(message):
-    if message.text == 'Назад в меню':
+    if test.back(bot, message):
         start(message)
     else:
         message_to_save_place = message.text
@@ -241,8 +190,7 @@ def place_1(message):
         fio = change_time_list[3]
         place = change_time_list[4]
         bot.send_message(message.chat.id, 'Обращение создано. Статус можно проверить перейдя по ссылке https://t.me/testpython100')
-        bot.send_message(id_chanel, f'Дата: {date}\nВремя: {time}\nБейдж: {number}\nСотрудник: {fio}\nМагазин: {place}\n')
-        # bot.send_message(message.chat.id, f'Дата: {date}\nВремя: {time}\nБейдж: {number}\nСотрудник: {fio}\nМагазин: {place}\n')
+        bot.send_message(id_chanel, f'Изменение времени смены\nДата: {date}\nВремя: {time}\nБейдж: {number}\nСотрудник: {fio}\nМагазин: {place}\n')
         start(message)
 @bot.message_handler(func=lambda m: m.text == 'Установить выходной день')
 def change(message):
@@ -253,22 +201,12 @@ def change(message):
     bot.register_next_step_handler(sent_1, date_2)
 
 def date_2(message):
-    total = []
-    if message.text == 'Назад в меню':
+    if test.back(bot, message):
         start(message)
     else:
-        message_to_save_date = message.text
-        for el in message_to_save_date:
-            if el.isdigit():
-                total.append(el)
-        if len(total) == 3:
-            total = total[0] + '.' + total[1] + total[2]
-            up_date_weekend.append(total)
-            sent_2 = bot.reply_to(message, 'Пожалуйста, свой бейдж сотрудника! 123456')
-            bot.register_next_step_handler(sent_2, number_myself_2)
-        elif len(total) == 4:
-            total = total[0] + total[1] +'.' + total[2] + total[3]
-            up_date_weekend.append(total)
+        if test.upload_date(bot, message):
+            upload_result = test.upload_date(bot, message)
+            up_date_weekend.append(upload_result)
             sent_2 = bot.reply_to(message, 'Пожалуйста, свой бейдж сотрудника! 123456')
             bot.register_next_step_handler(sent_2, number_myself_2)
         else:
@@ -276,20 +214,20 @@ def date_2(message):
             bot.register_next_step_handler(sent_5, date_2)
 
 def number_myself_2(message):
-    if message.text == 'Назад в меню':
+    if test.back(bot, message):
         start(message)
     else:
-        message_to_save_number = message.text
-        if message_to_save_number.isdigit():
-            up_date_weekend.append(message_to_save_number)
+         if test.upload_number(bot, message):
+            upload_result = test.upload_number(bot, message)
+            up_date_weekend.append(upload_result)
             sent_3 = bot.reply_to(message, 'Пожалуйста, укажите свою Фамилию и Имя! Иванова Анна')
             bot.register_next_step_handler(sent_3, fio_2)
-        else:
+         else:
             sent_3 = bot.reply_to(message, 'Пожалуйста, введите корректно табельный номер. 123456')
             bot.register_next_step_handler(sent_3, number_myself_2)
 
 def fio_2(message):
-    if message.text == 'Назад в меню':
+    if test.back(bot, message):
         start(message)
     else:
         message_to_save_fio_no_work = message.text.lower()
@@ -298,7 +236,7 @@ def fio_2(message):
         bot.register_next_step_handler(sent_3, place_2)
 
 def place_2(message):
-    if message.text == 'Назад в меню':
+    if test.back(bot, message):
         start(message)
     else:
         message_to_save_place = message.text
@@ -309,10 +247,9 @@ def place_2(message):
         fio = up_date_weekend[2]
         place = up_date_weekend[3]
         bot.send_message(message.chat.id, 'Обращение создано. Статус можно проверить перейдя по ссылке https://t.me/testpython100')
-        bot.send_message(id_chanel, f'Дата: {date}\nБейдж: {number}\nСотрудник: {fio}\nМагазин: {place}\n')
-        # bot.send_message(message.chat.id, f'Дата: {date}\nБейдж: {number}\nСотрудник: {fio}\nМагазин: {place}\n')
+        bot.send_message(id_chanel, f'Выходной\nДата: {date}\nБейдж: {number}\nСотрудник: {fio}\nМагазин: {place}\n')
         start(message)
 
-bot.polling()
-
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
 
